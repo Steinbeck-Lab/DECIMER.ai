5729d9363942ab55f003a7135ced29ca10f62125
Kohulan Rajan <kohulan.rajan@uni-jena.de>
2025-05-19 10:30:13 +0200

    feat: clean home page and about

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\ClipboardController;
-use App\Http\Controllers\DecimerController;
-use App\Http\Controllers\DecimerSegmentationController;
-use App\Http\Controllers\FileUploadController;
-use App\Http\Controllers\ProblemReportController;
-use App\Http\Controllers\ResultArchiveController;
-use App\Http\Controllers\SocketServerInitController;
-use App\Http\Controllers\StoutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileUploadController;
+use App\Http\Controllers\DecimerSegmentationController;
+use App\Http\Controllers\DecimerController;
+use App\Http\Controllers\StoutController;
+use App\Http\Controllers\ResultArchiveController;
+use App\Http\Controllers\ClipboardController;
+use App\Http\Controllers\ProblemReportController;
+use App\Http\Controllers\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,56 +19,31 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
- */
+*/
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
+Route::get('/', [FileUploadController::class, 'fileUpload'])->name('home');
+Route::post('/upload', [FileUploadController::class, 'fileUploadPost'])->name('file.upload.post');
 
-Route::get('/privacy_policy', function () {
-    return view('privacy_policy');
-})->name('privacy_policy');
+Route::get('/about', [AboutController::class, 'about'])->name('about');
 
-Route::get('/impressum', function () {
-    return view('impressum');
-})->name('impressum');
+Route::get('/DecIMER/segmentation', [DecimerSegmentationController::class, 'DecimerSegmentation'])->name('decimer.segmentation');
+Route::post('/DecIMER/segmentation', [DecimerSegmentationController::class, 'DecimerSegmentationPost'])->name('decimer.segmentation.post');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/DecIMER/OCSR', [DecimerController::class, 'DecimerOCSR'])->name('decimer.ocsr');
+Route::post('/DecIMER/OCSR', [DecimerController::class, 'DecimerOCSRPost'])->name('decimer.ocsr.post');
 
-/* Socket Server Initialisation Routes */
-Route::post('socket-init', [SocketServerInitController::class, 'socketInitPost'])->name('socket.init.post');
+Route::get('/STOUT/iupac', [StoutController::class, 'Stout'])->name('stout.iupac');
+Route::post('/STOUT/iupac', [StoutController::class, 'StoutPost'])->name('stout.iupac.post');
 
-/* File Upload Routes */
-Route::get('file-upload', [FileUploadController::class, 'fileUpload'])->name('file.upload');
-Route::post('file-upload', [FileUploadController::class, 'fileUploadPost'])->name('file.upload.post');
+Route::get('/download/results', [ResultArchiveController::class, 'archiveCreation'])->name('archive.creation');
+Route::post('/download/results', [ResultArchiveController::class, 'archiveCreationPost'])->name('archive.creation.post');
 
-/* File Clipboard Routes */
-Route::post('/clipboard-paste', [ClipboardController::class, 'store'])->name('clipboard.paste.post');
+Route::post('/clipboard/paste', [ClipboardController::class, 'store'])->name('clipboard.paste.post');
 
-/* DECIMER Segmentation Routes */
-Route::get('decimer-segmentation', [DecimerSegmentationController::class, 'DecimerSegmentation'])->name('decimer.segmentation');
-Route::post('decimer-segmentation', [DecimerSegmentationController::class, 'DecimerSegmentationPost'])->name('decimer.segmentation.post');
+Route::get('/problem/report', [ProblemReportController::class, 'ProblemReport'])->name('problem.report');
+Route::post('/problem/report', [ProblemReportController::class, 'ProblemReportPost'])->name('problem.report.post');
 
-/* DECIMER OCSR Routes */
-Route::get('decimer-ocsr', [DecimerController::class, 'DecimerOCSR'])->name('decimer.ocsr');
-Route::post('decimer-ocsr', [DecimerController::class, 'DecimerOCSRPost'])->name('decimer.ocsr.post');
-
-/* DECIMER OCSR Routes */
-Route::get('decimer-ocsr', [DecimerController::class, 'DecimerOCSR'])->name('decimer.ocsr');
-Route::post('decimer-ocsr', [DecimerController::class, 'DecimerOCSRPost'])->name('decimer.ocsr.post');
-
-/* Archive Generation Routes */
-Route::get('create-archive', [ResultArchiveController::class, 'archiveCreation'])->name('archive.creation');
-Route::post('create-archive', [ResultArchiveController::class, 'archiveCreationPost'])->name('archive.creation.post');
-
-/* STOUT V2 Routes */
-Route::get('stout-iupac', [StoutController::class, 'Stout'])->name('stout.iupac');
-Route::post('stout-iupac', [StoutController::class, 'StoutPost'])->name('stout.iupac.post');
-
-/* Problem Report Routes */
-Route::get('problem-report', [ProblemReportController::class, 'ProblemReport'])->name('problem.report');
-Route::post('problem-report', [ProblemReportController::class, 'ProblemReportPost'])->name('problem.report.post');
+Route::view('/privacy_policy', 'privacy_policy')->name('privacy_policy');
+Route::view('/impressum', 'impressum')->name('impressum');
 
 URL::forceScheme('https');
